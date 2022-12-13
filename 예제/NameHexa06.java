86a10dbc239013411d81f377c706aca9dbda950d
has8781 <has8781@gmail.com>
2022-12-13 20:48:54 +0900

    예제28

---
+++ 예제/NameHexa06.java
@@ -0,0 +1,7 @@
+public class NameHexa06 {
+    public static void main(String[] args) {
+        System.out.println("손 :" + Integer.toHexString('손'));
+        System.out.println("현 :" + Integer.toHexString('현'));
+        System.out.println("아 :" + Integer.toHexString('아'));
+    }
+}
